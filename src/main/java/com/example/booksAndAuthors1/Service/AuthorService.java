e58c02bf2878a53bde675416b701275a25cd8a2b
b0295994 <guptasiddharth2000@gmail.com>
2023-09-14 11:30:41 +0530

    Called webflux microservice from boot for get Books by author api.

--- src/main/java/com/example/booksAndAuthors1/Service/AuthorService.java
+++ src/main/java/com/example/booksAndAuthors1/Service/AuthorService.java
@@ -9,6 +9,7 @@ import com.example.booksAndAuthors1.models.Author;
 import com.example.booksAndAuthors1.models.Book;
 import com.example.booksAndAuthors1.repositories.AuthorRepository;
 import com.example.booksAndAuthors1.repositories.BookRepository;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AuthorService {
@@ -18,14 +19,20 @@ public class AuthorService {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+
     public Author saveAuthor(Author author) {
         return authorRepository.save(author);
     }
 
-    public List<Book> getBooksByAuthor( String name){
-        Author author = authorRepository.findByName(name);
-        String id = author.getId();
-        return bookRepository.findByAuthorId(id);
+    public List<Book> getBooksByAuthor(String name){
+//        Author author = authorRepository.findByName(name);
+//        String id = author.getId();
+//        return bookRepository.findByAuthorId(id);
+        String uri = "http://localhost:8080/webflux/books/findByAuthor?authorName="+name;
+        return restTemplate.getForObject(uri , List.class);
     }
 
     public List<Author> getAuthorsByNameRegex(String namesRegex) {
